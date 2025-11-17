0385ad219f0623ebfcd42a9bf557d4345415ef5b
ATSadi <ahnafsoad@gmail.com>
2025-11-18 02:56:39 +0600

    Welcome Page

--- src/main/java/com/example/ahnafsadi_cvbuilder/HelloController.java
+++ src/main/java/com/example/ahnafsadi_cvbuilder/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.ahnafsadi_cvbuilder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label welcomeLabel;
+
+    @FXML
+    void onClickCreateCv(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText("The CV form will be added in the next step.");
+        alert.showAndWait();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.ahnafsadi_cvbuilder {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.ahnafsadi_cvbuilder to javafx.fxml;
     exports com.example.ahnafsadi_cvbuilder;
 }
