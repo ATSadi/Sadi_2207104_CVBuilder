fb0503f194fb7ad2b6eb448141f9889efa2e3175
ATSadi <ahnafsoad@gmail.com>
2025-11-18 02:41:33 +0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ahnafsadi_cvbuilder {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ahnafsadi_cvbuilder to javafx.fxml;
+    exports com.example.ahnafsadi_cvbuilder;
+}
